e7bfa17292b227ca54fcff2ae0c31f76afca9c52
ETTARAK <ettarak.zouhair@gmail.com>
2021-08-27 10:03:04 +0100

    CRUD api

--- ApiVueJs/ApiVueJs/Controllers/DepartmentController.cs
+++ ApiVueJs/ApiVueJs/Controllers/DepartmentController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using ApiVueJs.Models;
 
 namespace ApiVueJs.Controllers
 {
@@ -48,5 +49,115 @@ namespace ApiVueJs.Controllers
 
             return new JsonResult(table);
         }
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            string query = @" SELECT DepartmentId, DepartmentName FROM dbo.Departments WHERE DepartmentId = @DepartmentId ";
+
+            DataTable table = new DataTable();
+
+            string v = _configuration.GetConnectionString("ApiVueJsConnection");
+            string sqlDataSource = v;
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department department)
+        {
+            string query = @" INSERT INTO dbo.Departments values (@DepartmentName) ";
+
+            DataTable table = new DataTable();
+
+            string v = _configuration.GetConnectionString("ApiVueJsConnection");
+            string sqlDataSource = v;
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+
+            return new JsonResult("Insert Department With SuccessFully ! ");
+        }
+
+
+        [HttpPut("{DepartmentId}")]
+        public JsonResult Put(Department department)
+        {
+            string query = @" Update dbo.Departments 
+                            SET DepartmentName = @DepartmentName 
+                             WHERE DepartmentId = @DepartmentId";
+
+            DataTable table = new DataTable();
+
+            string v = _configuration.GetConnectionString("ApiVueJsConnection");
+            string sqlDataSource = v;
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentId", department.DepartmentId);
+                    myCommand.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+
+            return new JsonResult("Update Department With SuccessFully ! ");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @" DELETE FROM dbo.Departments 
+                             WHERE DepartmentId = @DepartmentId";
+
+            DataTable table = new DataTable();
+
+            string v = _configuration.GetConnectionString("ApiVueJsConnection");
+            string sqlDataSource = v;
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Department With SuccessFully ! ");
+        }
+
     }
 }
